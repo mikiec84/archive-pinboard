f3181d72d96785008778e96406179138d55ccd77
Stephan Hügel <urschrei@gmail.com>
2013-04-01 17:21:24 +0100

    Adding HTTP 429 backoff functionality to respect TOS

--- archive-bookmarks.py
+++ archive-bookmarks.py
@@ -4,14 +4,44 @@
 import os
 import sys
 from datetime import datetime
+import time
 import requests
+
+
+def retrieve():
+    """ Get all the posts from Pinboard """
+    req = requests.get(
+        pinboard_api + 'posts/all',
+        params=payload)
+    # raise an exception for a 4xx code, unless it's rate-limiting
+    try:
+        req.raise_for_status()
+    except requests.HTTPError:
+        if req.status_code == 429:
+            delay = wait.next()
+            print("Waiting %ss because of rate-limiting" % delay)
+            time.sleep(delay)
+            retrieve()
+        else:
+            raise
+    return req.text
+
+
+def backoff():
+    """ Return an exponential backoff period """
+    delay = 2
+    while True:
+        if delay >= 300:
+            print("Exceeded max. backoff period of 5 minutes. Try again later")
+            brutal_error_handler()
+        yield delay
+        delay = delay * 2
 
 
 def brutal_error_handler():
     """ Silly, simple error handling. Never do this. """
     print("Something went horribly wrong, so we're not continuing")
     sys.exit(1)
-
 
 # Parameters.
 bookmarkdir = os.path.join(os.environ['HOME'], 'Dropbox/Personal/pinboard')
@@ -49,18 +79,14 @@ if not os.path.exists(outdir):
         print("Couldn't create a directory at %s" % outdir)
         brutal_error_handler()
 
-# Get all the posts from Pinboard
-req = requests.get(
-    pinboard_api + 'posts/all',
-    params=payload)
-# raise an exception for a 4xx code
-req.raise_for_status()
+wait = backoff()
+bookmarks = retrieve()
 print("Authentication successful, trying to write backup.")
 
 # write a new bookmarks file
 try:
     with open(os.path.join(outdir, 'pinboard-backup_' + t + '.xml'), 'w') as o:
-        o.write(req.text.encode("utf-8"))
+        o.write(bookmarks.encode("utf-8"))
 except IOError:
     print("Couldn't create new bookmarks file at %s" % outdir)
     brutal_error_handler()
